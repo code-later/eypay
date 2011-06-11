a59e9c6cbed1903d3a953e959533ddc6ebbef4ec
Björn Vollmer <bjoern.vollmer@gmail.com>
2011-06-11 15:03:24 +0200

    Improved configuration interface

--- lib/eypay/engine.rb
+++ lib/eypay/engine.rb
@@ -2,5 +2,9 @@ module Eypay
   require "eypay"
   require "rails"
   class Engine < Rails::Engine
+    config.eypay            = ActiveSupport::OrderedOptions.new
+    config.eypay.qpay_url   = "https://secure.wirecard-cee.com/qpay/init.php"
+    config.eypay.currency   = "EUR"
+    config.eypay.language   = "de"
   end
 end
